0bb9b63323d308808db60bc296ebfabb0eba02ee
Martin Page <wookoouk@gmail.com>
2016-09-07 12:42:42 +0100

    updated auto-lebab

--- lebab.sh
+++ lebab.sh
@@ -2,6 +2,6 @@
 for file in $(find . -name "*.js"); do
 if [[ $file != *"public/"* ]] && [[ $file != *"node_modules/"* ]]; then
 #echo $file
-    lebab $file -o $file
+    lebab $file -o $file --transform arrow,for-of,arg-spread,obj-method,obj-shorthand,no-strict,exponent
 fi
 done
