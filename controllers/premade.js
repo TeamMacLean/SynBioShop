6ac194032fabcc8c6f845a20424be7eb701941b8
Martin Page <wookoouk@gmail.com>
2017-01-27 13:01:55 +0000

    test

--- controllers/premade.js
+++ controllers/premade.js
@@ -36,16 +36,21 @@ premade.rearrange = (req, res) => {
     DB.getJoin({categories: true}).then(dbs=> {
 
 
+        // Type.getByCategory()
+
+
         //TODO prune data
 
 
-        var pruned = dbs.map(db=>{
-            var cats = db.categories.map(cat=>{
-                return {id:cat.id,position:cat.position, name:cat.name};
-            })
-            return {categories:cats, id:db.id, name:db.name, position:db.position};
-        });
+        var pruned = dbs.map(db=> {
+            var cats = db.categories.map(cat=> {
 
+                //TODO get items too
+
+                return {id: cat.id, position: cat.position, name: cat.name};
+            });
+            return {categories: cats, id: db.id, name: db.name, position: db.position};
+        });
 
 
         return res.render('premade/rearrange', {dbs: pruned});
