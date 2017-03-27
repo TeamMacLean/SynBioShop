6ecf7d4d08efdb5136e3f09171de940725e2d783
Martin Page <wookoouk@gmail.com>
2017-03-27 10:34:37 +0100

    Loc

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const util = require('./lib/util.js');
 const routes = require('./routes');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(require('less-middleware')(path.join(__dirname, 'public', 'style')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
