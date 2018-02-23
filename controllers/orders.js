c75f3a171fb9dff318e5e670d0fe10611f39034c
Martin Page <wookoouk@gmail.com>
2018-02-23 15:26:11 +0000

    test

--- controllers/orders.js
+++ controllers/orders.js
@@ -93,9 +93,9 @@ orders.simonRepeatOrders = (req, res) => {
 
                     promises.push(new Promise((good, bad) => {
                         item.getType().then((type) => {
-                            console.log('type', type);
                             addItem(o.username, type.name);
                         }).catch(err => {
+                            //TODO if not found it probebly doesnt exist any more
                         });
                     }));
                 });
