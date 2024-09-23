a65516104e9ac4fe8e68d0fa586299fb671edb63
George Deeks <georgedeeks@gmail.com>
2024-09-23 10:55:06 +0100

    fix: add timeout for budgethodlers response

--- controllers/shoppingCart.js
+++ controllers/shoppingCart.js
@@ -17,40 +17,35 @@ const pricePerUnit = config.pricePerUnit;
 
 const ShoppingCart = {};
 
-
 ShoppingCart.index = (req, res) => {
   const username = req.user.username;
 
-  if (username === 'deeks'){
+  if (username === 'deeks') {
     req.user.company = 'JIC';
   }
 
   const curlCommandStr = 
-    'curl -s -H "' + 
+    'curl --max-time 4 -s -H "' + // Added --max-time 5 for a 5-second timeout
     'Authorization: ' + 
     config.lookupBudget.headers.authorization + 
     '" -H "Cookie: ' + 
     config.lookupBudget.headers.cookie + 
     '" ' + '-H "Accept: application/json, text/plain, */*" -H "User-Agent: axios/1.7.2" -H "Accept-Encoding: gzip, compress, deflate, br" -H "Connection: close" ' + 
-    config.lookupBudget.url
-  ;
+    config.lookupBudget.url;
 
   exec(curlCommandStr, (error, stdout, stderr) => {
     if (error) {
       console.error('stderr:', stderr);
       console.error('Failed to fetch budget holders:', error.message);
-      return continueWithCartOperations([]);
+      // Redirect to an error page or send an error message
+      return res.redirect('/error'); // Redirects to a generic error page
     } else {
-      
-      //console.log('success! budget holders found', stdout);
-      
       const html = stdout;
-      const $ = cheerio.load(html);    
+      const $ = cheerio.load(html);
       const budgetHolders = [];
-      $('select[name="BH"] option').each(function() {
+      $('select[name="BH"] option').each(function () {
         const value = $(this).attr('value');
         const name = $(this).text().trim();
-        // Ensure the value is defined and it's not the placeholder option
         if (value && name !== 'SELECT BUDGET HOLDER') {
           budgetHolders.push({
             username: value,
@@ -58,8 +53,6 @@ ShoppingCart.index = (req, res) => {
           });
         }
       });
-
-      //console.log('First 5 budgetHolders:', budgetHolders.slice(0, 5));
       continueWithCartOperations(budgetHolders);
     }
   });
@@ -71,45 +64,39 @@ ShoppingCart.index = (req, res) => {
           cart.items = [];
         }
 
-        const promises = cart.items.map(
-          (item) =>
-            new Promise((resolve, reject) => {
-              item
-                .getType()
-                .then((type) => {
-                  item.type = type;
-                  return resolve(item);
-                })
-                .catch((err) => {
-                  return reject(err);
-                });
-            })
+        const promises = cart.items.map((item) =>
+          new Promise((resolve, reject) => {
+            item.getType()
+              .then((type) => {
+                item.type = type;
+                return resolve(item);
+              })
+              .catch((err) => {
+                return reject(err);
+              });
+          })
         );
+        
         Promise.all(promises)
           .then((updatedItems) => {
             cart.items = [].concat(...updatedItems);
 
             const isAdmin = config.admins.includes(username);
-
-            //const force = (isAdmin && req.query.adminForceShowPricing === 'true') || false;
             const force = true; // temp testing
+            const adminButtonText = req.query.adminForceShowPricing === 'true' ? 'Disable Pricing View' : 'Enable Pricing View';
 
-            const adminButtonText = (req.query.adminForceShowPricing === 'true') ? 'Disable Pricing View' : 'Enable Pricing View';
+            console.log('frontend receiving this many budget holders:', budgetHolders.length);
 
-            console.log('frontend receiveing this many budgetholders:', budgetHolders.length)
-
-            return res.render("cart/index", { 
-              cart, 
-              pricePerUnit, 
-              forceShowPricing: force, 
-              adminButtonText, 
-              isAdmin, 
-              budgetHolders, 
+            return res.render('cart/index', {
+              cart,
+              pricePerUnit,
+              forceShowPricing: force,
+              adminButtonText,
+              isAdmin,
+              budgetHolders
             });
           })
-          .catch((err) => {
-            return renderError(err, res);
-          });
+          .catch((err) => renderError(err, res));
       })
       .catch((err) => renderError(err, res));
   }
