a3ae4eeab4fde97d3c3bc2711bbdfbdafed099ac
George Deeks <georgedeeks@gmail.com>
2025-01-24 14:55:58 +0000

    fix: new db has all type options

---
+++ temp_hardcoded_types_edit.js
@@ -0,0 +1,17 @@
+Type.types = [
+  {
+    fields: [ [Object], [Object], [Object], [Object], [Object], [Object] ],
+    typeName: 'Comments, Inside Overhangs, Outside overhangs, Description, Resistance, Who Made it',
+    type: 1,
+  },
+  {
+    fields: [ [Object], [Object], [Object] ],
+    typeName: 'Code, Comments, Description, Species of Origin, Who Made it',
+    type: 2,
+  },
+  {
+    fields: [ [Object], [Object], [Object], [Object], [Object] ],
+    typeName: 'Vector Name, Comments, Module Description, 5′ OH on 5′ Strand, 3′ OH on 5′ Strand, Level 1 Position, Selection, Source',
+    type: 3,
+  }
+]
